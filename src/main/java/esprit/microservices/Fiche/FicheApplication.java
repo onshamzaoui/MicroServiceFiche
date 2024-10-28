42208f2f5c3053323586a6a97b302dda77c6379c
onshamzaoui <onshamzaoui720@gmail.com>
2024-10-28 18:08:00 +0100

    first commit

---
+++ src/main/java/esprit/microservices/Fiche/FicheApplication.java
@@ -0,0 +1,13 @@
+package esprit.microservices.Fiche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicheApplication.class, args);
+	}
+
+}
